dc0d30ade4feb6afd329f2a5fc9399350914c6c2
AS <arunsalgia@gmail.com>
2021-06-01 18:48:03 +0530

    correction in dummy

--- APL/src/views/APL/Dummy.js
+++ APL/src/views/APL/Dummy.js
@@ -1,5 +1,5 @@
 import React, { useState ,useContext} from 'react';
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 //import Button from '@material-ui/core/Button';
 //import CssBaseline from '@material-ui/core/CssBaseline';
 //import Typography from '@material-ui/core/Typography';
