48be8b582dd250bdcbd95643a98650c6ace0da5c
AS <arunsalgia@gmail.com>
2021-06-04 11:19:05 +0530

    server ping removed from schedule

--- routes/playerstat.js
+++ routes/playerstat.js
@@ -1733,13 +1733,14 @@ async function pingServer() {
 // schedule task 
 cron.schedule('*/1 * * * *', () => {
 
-  if ((WEB) && (PRODUCTION)) {   // not to be done in APK
-    if (++pingTimer >= PINGUPDATEINTERVAL) {
-      console.log("Pinging Server");
-      pingTimer = 0;
-      pingServer();
-    }
-  }
+
+  // if ((WEB) && (PRODUCTION)) {   // not to be done in APK
+  //   if (++pingTimer >= PINGUPDATEINTERVAL) {
+  //     console.log("Pinging Server");
+  //     pingTimer = 0;
+  //     pingServer();
+  //   }
+  // }
 
   if (!db_connection) {
     //console.log("============= No mongoose connection");
