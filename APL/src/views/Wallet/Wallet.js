5092815263838a06a09c65ebb966460d1c18740d
AS <arunsalgia@gmail.com>
2021-06-08 09:01:28 +0530

    Wallet widthdraw button text chnaged

--- APL/src/views/Wallet/Wallet.js
+++ APL/src/views/Wallet/Wallet.js
@@ -140,7 +140,7 @@ export default function Wallet(props) {
       </Button>
       <Button type="submit" variant="contained" color="primary" 
        onClick={handleWithdraw}
-        className={gClasses.button}>Withdraw from Wallet
+        className={gClasses.button}>Withdraw
      </Button>
      </div>
     )
